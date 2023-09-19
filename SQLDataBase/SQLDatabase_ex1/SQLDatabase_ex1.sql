384fd181e833456dc22a5ab300c77530972c714e
njh8810 <njh8810@naver.com>
2023-09-19 13:53:31 +0900

    MSSQL 기본키 제약조건 및 인덱스 관련 내용 커밋

--- SQLDataBase/SQLDatabase_ex1/SQLDatabase_ex1.sql
+++ SQLDataBase/SQLDatabase_ex1/SQLDatabase_ex1.sql
@@ -51,3 +51,140 @@ INSERT INTO dbo.tbl_primarykey values (1002, 2, 301)
 
 
 select * from tbl_primarykey
+
+
+use [SalesSimple]
+-- DB 내 인덱스 확인
+exec sp_helpIndex customers;
+execute sp_helpIndex customers;
+
+--NONCLUSTERED 인덱스
+--테이블내 NONCLUSTERED 인덱스는 여러개를 생성할 수 있다.
+--PRIMARY KEY로 지정된 열이 아닌 다른 열(컬럼) UNIQUE를 지정해주면 해당 열에 대해서는 NONCLUSTERED 인덱스가 적용
+
+
+
+-- CLUSTERED INDEX VS NONCLUSTERED INDEX
+
+CREATE TABLE dbo.tbl_customers
+(
+	cus_id	CHAR(10)	NOT	NULL	PRIMARY	KEY,
+	cus_name	NVARCHAR(20)	NOT NULL	UNIQUE,
+	cus_class	NVARCHAR(20)	NOT NULL	UNIQUE,
+)
+
+
+exec sp_helpIndex [dbo.tbl_customers];
+
+--insert
+insert into dbo.tbl_customers values('CCC', '홍길동', '씨앗1');
+insert into dbo.tbl_customers values('BBB', '장길동', '씨앗2');
+insert into dbo.tbl_customers values('AAA', '김길동', '씨앗3');
+
+--select
+select * from dbo.tbl_customers
+
+--insert2
+insert into dbo.tbl_customers values('YYY', '황길동', '씨앗4');
+insert into dbo.tbl_customers values('DDD', '조길동', '씨앗5');
+insert into dbo.tbl_customers values('에이에이', '노길동', '씨앗6');
+insert into dbo.tbl_customers values('홍길동', '광길동', '씨앗7');
+insert into dbo.tbl_customers values('광길동', '길동', '씨앗8');
+
+--한글 영문 우선순위(1)
+--기본적으로 한글이 우선순의를 갖는다(한글 완성형 기준)
+--이유는 데이터베이스의 기본데이터 정렬 속성이 Korean_Wansung_CI_AS(한글 완성형 기준)로 되어 있기 때문
+--그러나 우선순위를 변경하는 것도 가능하다.
+--Collation 정보
+-- => SQL Server의 기본 데이터 정렬을 설정하는 옵션
+-- => DB 설치시에 셋팅
+
+--MSSQL 을 설치할때 대부분 Korean_Wansung_CI_AS로 설정
+-- => 한국어(한글) 완성형 문자라는 의미
+
+--Collation 정보 보기
+-- => GUI 모드에서 바로 확인 가능
+SELECT * FROM ::fn_helpcollations() where name like 'latin%'
+
+--Collation 정보 변경
+-- => ALTER 명령어를 사용
+-- => 기본키 제약조건 등으로 안될 수 있으며 초보자는 DB 백업을 권한다.
+
+--ALTER DATABASE 디비명 COLLATE Korean_Wansung_CI_AI(Latin1_General_CI_AS)
+
+
+
+-- CREATE DATABASE
+CREATE DATABASE [9TO9];
+
+-- CHANGE DB
+USE [9TO9];
+
+
+-- CREATE TABLE
+CREATE TABLE dbo.tbl_customers
+(
+	cus_id	CHAR(10)	NOT	NULL	PRIMARY	KEY,
+	cus_name	nvarchar(20)	NOT NULL UNIQUE,
+	cus_class	nvarchar(20)	NOT NULL UNIQUE
+)
+
+
+--insert
+INSERT into dbo.tbl_customers values('CCC','홍길동','씨앗1')
+INSERT into dbo.tbl_customers values('BBB','김길동','씨앗2')
+INSERT into dbo.tbl_customers values('AAA','장길동','씨앗3')
+INSERT into dbo.tbl_customers values('홍길동','HONG','씨앗4') -- 한글입력
+INSERT into dbo.tbl_customers values('박길동','PARK','씨앗5')
+INSERT into dbo.tbl_customers values('강길동','KANG','씨앗6')
+
+
+select * from dbo.tbl_customers order by cus_id 
+
+
+--collations 정보보기
+SELECT * FROM ::fn_helpcollations() where name like 'latin%'
+
+
+
+-- 기본 데이터 정렬 속성값 변경(COLLATE 쿼리문 실습)
+--[1]
+-- DATABASE COLLATIOM 정보 보기
+--DATABASE COLLATION 보기
+SELECT DATABASEPROPERTYEX('9TO9', 'collation') AS COLLATION
+
+--데이터베이스 기본 데이터 정렬 속성 변경
+--테이블(tbl_customers) 내 컬럼 기본 데이터 정렬 속성 변경
+
+
+--[2]
+-- DATABASE > TABLE COLLATION 정보보기
+exec sp_help tbl_customers
+execute sp_help tbl_customers
+sp_help tbl_customers
+exec sp_help [dbo.tbl_customers]
+exec sp_help 'dbo.tbl_customers'
+exec sp_help "dbo.tbl_customers"
+
+--[3]
+--기본 데이터 정렬 속성 변경(DB)
+--데이터베이스 COLLATION 정보를 변경한다는 것은 '기본 데이터 정렬' 속성을 변경한다는 뜻이다.
+--그렇게 되면 변경 이후에 생성되는 신규 테이블들은 변경된 '기본 데이터 정렬' 속성을 따라서 생성되게 된다.
+--물론 변경전에 생성된 테이블들의 속성은 변하지 않는다. 생성시의 속성 상태가 유지된다.
+--ALTER
+
+ALTER DATABASE [9TO9] COLLATE Korean_Wansung_CI_AS
+
+SELECT DATABASEPROPERTYEX('9TO9', 'collation') as collation
+
+
+--ALTER(TABLE > COLUMN)
+ALTER TABLE dbo.tbl_customers ALTER COLUMN cus_id CHAR(10) COLLATE Korean_Wansung_CI_AS
+ALTER TABLE dbo.tbl_customers ALTER COLUMN cus_class nvarCHAR(20) COLLATE Korean_Wansung_CI_AS
+ALTER TABLE dbo.tbl_customers ALTER COLUMN cus_name nvarCHAR(20) NOT NULL
+
+
+exec sp_helpindex [dbo.tbl_customers]
+
+
+
---
+++ SQLDataBase/SQLDatabase_ex1/SQLDatabase_ex2.sql
@@ -0,0 +1,135 @@
+﻿--PK/UK 제약조건 추가/삭제
+--PRIMARY KEY CONSTRAINT ADD/DROP
+--UNIQUE CONSTRAINT ADD/DROP
+--먼저 제약조건(PRIMARY KEY, UNIQUE) 없이 테이블 생성
+--제약조건 추가 및 확인
+--제약조건 삭제 및 확인
+--테이블 삭제
+
+
+CREATE DATABASE [9TO9]
+
+--작업 DB 변경
+USE [9TO9]
+
+
+
+------------------------------------------------------------
+-- [!] PRIMARY KEY CONSTRAINT ADD / DROP
+------------------------------------------------------------
+-- [1] 테이블 생성
+-- 먼저 기본키 제약조건, 유니크 제약조건 없이 테이블을 생성해본다.
+
+CREATE TABLE dbo.tbl_customers4
+(
+	cus_id CHAR(10) NOT NULL,
+	cus_name NVARchar(20) NOT NULL,
+	cus_class NVARchar(20) NOT NULL,
+)
+
+-- [2] 인덱스 확인
+-- 기본키 지정이나 유니크 지정을 안했기 때문에 인덱스 확인을 해보면 없다라고 나온다.
+sp_helpindex [dbo.tbl_customers4]
+sp_helpconstraint tbl_customers4
+
+
+-- [3] 기본키 추가(제약조건 추가)
+exec sp_helpconstraint tbl_customers4
+
+-- CLUSTERED는 안써줘도 기본으로 클러스터드 인덱스 생성해줌.
+ALTER TABLE dbo.tbl_customers4 
+	ADD CONSTRAINT PK_tbl_customers4 PRIMARY KEY CLUSTERED (cus_id);
+GO
+
+EXEC sp_helpindex tbl_customers4
+GO
+
+--[4] 기본키 제약조건 삭제
+ALTER TABLE dbo.tbl_customers4 DROP CONSTRAINT PK_tbl_customers4
+GO
+
+EXEC sp_helpindex tbl_customers4
+GO
+
+
+--[5] 테이블 삭제
+DROP TABLE dbo.tbl_customers4
+
+
+
+
+------------------------------------------------------------
+-- [!] UNIQUE CONSTRAINT ADD / DROP
+------------------------------------------------------------
+
+--작업 DB 변경
+USE [9TO9]
+
+--[1] 테이블 생성
+CREATE TABLE dbo.tbl_customers4
+(
+	cus_id CHAR(10) NOT NULL,
+	cus_name NVARchar(20) NOT NULL,
+	cus_class NVARchar(20) NOT NULL,
+)
+
+--[2]인덱스 확인
+exec sp_helpindex [dbo.tbl_customers4]
+
+--[3]UNIQUE 추가(제약조건 추가)
+ALTER TABLE dbo.tbl_customers4 
+	ADD 
+		CONSTRAINT UQ_tbl_customers4 UNIQUE NONCLUSTERED(cus_name)
+GO
+
+ALTER TABLE dbo.tbl_customers4
+	DROP	
+		CONSTRAINT UQ_tbl_customers4
+GO
+
+--[4]
+DROP TABLE [tbl_customers4]
+
+-----------------------------------------------------------------------------------
+--기본키 삭제 및 확인
+--기본키 삭제 및 확인하는 다른 방법들
+-----------------------------------------------------------------------------------
+--A
+ALTER TABLE dbo.tbl_customers4 DROP CONSTRAINT PK_tbl_customers4
+GO
+
+--B
+ALTER TABLE dbo.tbl_customers4
+	DROP PK_tbl_customers4
+
+exec sp_helpconstraint [dbo.tbl_customers4]
+
+--기본키 확인(시스템 뷰 - 정보 스키마 뷰)
+--데이터베이스 뷰쪽에 DB마다 생성이 되어 있다.
+--테이블 각 컬럼에 대한 정보를 보거나 활용하고자 할 때 사용할 수 있다.
+--조인 등을 통하여 여러 정보를 묶어서 활용할 수 도 있다.
+
+select * from INFORMATION_SCHEMA.KEY_COLUMN_USAGE where TABLE_NAME = 'tbl_customers4'
+
+------------------------------------------------------------------------------------
+--[!] 기본키(제약조건) 존재 시 삭제하기
+------------------------------------------------------------------------------------
+use [9TO9]
+
+--[1] 테이블 생성
+CREATE TABLE dbo.tbl_customers6
+(
+	cus_id	CHAR(10)	NOT NULL	PRIMARY	KEY,
+	cus_name	NVARCHAR(20)	NOT	NULL	UNIQUE,
+	cus_class	NVARCHAR(20)	NOT	NULL	UNIQUE,
+)
+
+exec sp_helpconstraint [dbo.tbl_customers6]
+
+alter table dbo.tbl_customers6
+	drop constraint
+		PK__tbl_cust__E84D41E8B55E04F4
+
+alter table dbo.tbl_customers6
+	add constraint
+		PK_tbl_customers6_cus_id primary key clustered(cus_id)
